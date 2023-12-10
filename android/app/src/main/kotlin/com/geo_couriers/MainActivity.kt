e0692af5ec52c570b91e65d5767cd657820141f4
lazo <lazarekvirtia@gmail.com>
2023-12-10 18:22:12 +0400

    refactoring

--- android/app/src/main/kotlin/com/geo_couriers/MainActivity.kt
+++ android/app/src/main/kotlin/com/geo_couriers/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.geo_couriers
 
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity: FlutterFragmentActivity() {
+import io.flutter.embedding.android.FlutterActivity
+class MainActivity: FlutterActivity() {
 }
